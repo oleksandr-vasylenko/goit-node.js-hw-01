83b8d9d10dcacfd5ba7d896ba271994ef7761ce9
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-02-11 13:23:49 +0200

    Sets up repository

---
+++ contacts.js
@@ -0,0 +1,33 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
