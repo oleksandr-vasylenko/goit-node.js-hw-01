1e4e8f43e18d7e6bec63a507d29b925a7d4201b3
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2023-02-12 20:56:09 +0200

    Adds listContacts function

--- contacts.js
+++ contacts.js
@@ -1,21 +1,30 @@
 const fs = require("fs").promises;
 const path = require("path");
-
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 const listContacts = async () => {
   try {
     const data = await fs.readFile(contactsPath);
     const contacts = JSON.parse(data);
-    return contacts;
+    return console.log(contacts);
   } catch (error) {
     console.log(error);
   }
 };
 
-function getContactById(contactId) {
-  // ...твій код
-}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+
+    contacts.find((item) => {
+      item.id === contactId;
+      return console.log(item.name);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 function removeContact(contactId) {
   // ...твій код
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("hello node.js");
+// index.js
+const argv = require("yargs").argv;
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      //   console.log("id", id);
+      getContactById(id);
+      break;
+
+    case "add":
+      console.log("name email phone", name, email, phone);
+      break;
+
+    case "remove":
+      console.log("id", id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
